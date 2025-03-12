8fe61346b9db59315ba6e19d6f3ec69826c741ba
rajukunarapu <rajukunarapu49@gmail.com>
2025-03-12 10:00:38 +0530

    developing stage

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+const HeaderComponent = () => {
+    return (
+        <div className="nav-container">
+            <div className="Logo-container">
+                <img className="logo" src="https://vectorseek.com/wp-content/uploads/2023/10/Swiggy-Black-Logo-Vector.svg-.png" alt="" />
+            </div>
+            <div className="HeadingContainer">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderComponent;
